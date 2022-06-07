f67e2d780e833a7c8adc44f36cd754f559dadfad
Gabrielitogg <101334114+Gabrielitogg@users.noreply.github.com>
2022-06-07 01:38:28 -0400

    Add files via upload

---
+++ ProyectoCineflix/cineflix/core/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+
+import core
+from .views import index, lomasnuevo, recomendadas , ranking, categoria , pelicula , peliculasmostrar , registrarpelicula , editarpeliculas ,eliminarpeliculas
+urlpatterns = [
+    path('', index, name="index"),
+    path('lomasnuevo', lomasnuevo, name="lomasnuevo"),
+    path('recomendadas', recomendadas, name="recomendadas"),
+    path('ranking', ranking, name="ranking"),
+    path('categoria/<int:id>', categoria, name="categoria"),
+    path('pelicula/<int:id>', pelicula, name="pelicula"),
+    path('peliculasmostrar', peliculasmostrar , name = "peliculasmostrar"),
+    path('registrar' , registrarpelicula , name = "registrar" ),
+    path('editarpeliculas' , editarpeliculas , name = "editarpeliculas" ),
+     path('eliminarpeliculas/<int:id>',eliminarpeliculas , name="eliminarpeliculas"),
+]
